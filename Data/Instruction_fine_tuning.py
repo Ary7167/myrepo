94a55aef9ae165502933ef6a50bda1d73f6df237
Aryan Koganti <93660238+Ary7167@users.noreply.github.com>
2024-05-21 15:48:20 +0530

    Update Instruction_fine_tuning.py

--- Data/Instruction_fine_tuning.py
+++ Data/Instruction_fine_tuning.py
@@ -1,1 +1,6 @@
-
+# Dataloader for instructon fine tuning data 
+# Developed by referencing the wandb article on instruction fine tuning LLMs
+import wandb
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
