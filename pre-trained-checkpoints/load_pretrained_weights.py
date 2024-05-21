ac1a9e3301e24b4cd086f0252b159846ab2549e3
Aryan Koganti <93660238+Ary7167@users.noreply.github.com>
2024-05-21 11:08:14 +0530

    Update load_pretrained_weights.py

--- pre-trained-checkpoints/load_pretrained_weights.py
+++ pre-trained-checkpoints/load_pretrained_weights.py
@@ -1,1 +1,4 @@
 # load pre-trained weights of the GPT-2 model in place of pretraining text
+# Pre-training on unlabeled data might require significant amount of compute (atleast A100 GPUs). In such cases we adopt to the pre-trained weights of the GPT-2
+# available open-source from the Open-AI repo.
+
