eef81827ed13a2eb662ead46d49e5ae3c7d08880
Aryan Koganti <93660238+Ary7167@users.noreply.github.com>
2024-05-28 15:20:38 +0530

    Update instruction_tune.py

--- Fine_tune/instruction_tune.py
+++ Fine_tune/instruction_tune.py
@@ -1,1 +1,21 @@
 # script for instruction tuning LLM for answering user queries 
+import wandb
+import torch
+import torch.nn as nn
+
+# we import our custom instruction tuning dataset and fine-tune with the help of the wandb server for convenience
+
+file_path= 'myrepo/Dataset/instruction_tuning.json'
+with open(file_path,'r') as f:
+     data= json.load(f)
+
+with wandb.init(project='instruction-tuning'):
+     at= wandb.artifact(
+                 name="instruction_tuning"
+                 type="dataset"
+                 description="instruction tuning dataset for question answering ability "
+                 metadata= {"https://github.com/Ary7167/myrepo/Dataset"}
+            )
+     at.add_file("instruction_tuning.json")
+
+     
