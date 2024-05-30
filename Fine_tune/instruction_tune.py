42fd47be6d14c72916c00d7499cb7f4cbf8d7c4b
Aryan Koganti <93660238+Ary7167@users.noreply.github.com>
2024-05-30 15:49:40 +0530

    Update instruction_tune.py

--- Fine_tune/instruction_tune.py
+++ Fine_tune/instruction_tune.py
@@ -1,21 +1,29 @@
 # script for instruction tuning LLM for answering user queries 
 import wandb
+import accelerate
+import trl 
+import Datasets
+import os
 import torch
-import torch.nn as nn
+from dataset import load_dataset
+from trl import SFTTrainer
 
 # we import our custom instruction tuning dataset and fine-tune with the help of the wandb server for convenience
+batch_size = 16
+num_workers = os.cpu_count()
+max_steps = 3000
+bf16 = False
+fp16 = True
+gradient_accumulation_steps = 2
+context_length = 256
+logging_steps = 500
+save_steps = 500
+learning_rate = 0.0001
+model_name = 't'
+out_dir = 'outputs/gpt2_alpaca_preprocess_fn'
 
-file_path= 'myrepo/Dataset/instruction_tuning.json'
-with open(file_path,'r') as f:
-     data= json.load(f)
+#load the instruction tuning dataset
+instruction_tune_data= Dataloader('instruction_tune.json')
 
-with wandb.init(project='instruction-tuning'):
-     at= wandb.artifact(
-                 name="instruction_tuning"
-                 type="dataset"
-                 description="instruction tuning dataset for question answering ability "
-                 metadata= {"https://github.com/Ary7167/myrepo/Dataset"}
-            )
-     at.add_file("instruction_tuning.json")
-
+# preprocess and tokenize the dataset using the byte pair tokenizer
      
