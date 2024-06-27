321248ffe7f1c525e350004c08f5e9b7195dacf9
Aryan Koganti <93660238+Ary7167@users.noreply.github.com>
2024-06-27 11:25:10 +0530

    Create RAG.py

---
+++ RAG.py
@@ -0,0 +1,10 @@
+# 1.) Document loader 
+# 2.) Document Splitter
+# 3.) Embeddings and storage in vectorDB
+# 4.) Post processing and prompt similarity using cosine similarity
+# using langchain and langsmith 
+
+import langchain
+import langsmith
+
+# load the documents from the trusted source
